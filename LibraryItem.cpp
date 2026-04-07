94b0b2b174113ccb25d18664a9aac035b80a2a5e
Awab Musa <awabmusaabdalla@gmail.com>
2026-04-07 13:24:41 +0200

    Add constructor and destructor

--- LibraryItem.cpp
+++ LibraryItem.cpp
@@ -16,3 +16,28 @@ bool LibraryItem::getIsBorrowed()
 {
     return isBorrowed;
 }
+
+void LibraryItem::setId(int newId)
+{
+    id = newId;
+}
+
+void LibraryItem::setTitle(string newTitle)
+{
+    title = newTitle;
+}
+
+void LibraryItem::setIsBorrowed(bool newIsBorrowed)
+{
+    isBorrowed = newIsBorrowed;
+}
+
+LibraryItem::LibraryItem(int id, string title, float price) : id(id), title(title), price(price)
+{
+    totalItems++;
+}
+
+LibraryItem::~LibraryItem()
+{
+    totalItems--;
+}
