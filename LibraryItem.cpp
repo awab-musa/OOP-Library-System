2b1345622de76414bacaa0accfb536076927a41b
Awab Musa <awabmusaabdalla@gmail.com>
2026-04-07 13:19:46 +0200

    Implement getters

--- LibraryItem.cpp
+++ LibraryItem.cpp
@@ -1,3 +1,18 @@
 #include "LibraryItem.h"
 
 int LibraryItem::totalItems = 0;
+
+int LibraryItem::getId()
+{
+    return id;
+}
+
+string LibraryItem::getTitle()
+{
+    return title;
+}
+
+bool LibraryItem::getIsBorrowed()
+{
+    return isBorrowed;
+}
