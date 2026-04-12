aac1ec20a3ab0570311e8393c4dcfcb55e0f32f6
awab-musa <awabmusaabdalla@gmail.com>
2026-04-12 22:04:41 +0200

    Implement ResearchPaper constructor

--- ResearchPaper.cpp
+++ ResearchPaper.cpp
@@ -1,1 +1,7 @@
 #include "ResearchPaper.h"
+#include <iostream>
+using namespace std;
+ResearchPaper::ResearchPaper(int id, string title, float price, string researcher, string field)
+    : LibraryItem(id, title, price), researcher(researcher), field(field)
+{
+}
