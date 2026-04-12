ef6319b617a774d93ae51ee936fb92f62f929a50
Awab Musa <awabmusaabdalla@gmail.com>
2026-04-12 13:56:59 +0200

    Implement Book constructor

--- Book.cpp
+++ Book.cpp
@@ -1,3 +1,7 @@
 #include "Book.h"
 #include <iostream>
 using namespace std;
+Book::Book(int id, string title, float price, string author, int pages)
+    : LibraryItem(id, title, price), author(author), pages(pages)
+{
+}
