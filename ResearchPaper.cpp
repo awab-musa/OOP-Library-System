105d0fe48c5dde07d72e744f155f1d92fe995b12
awab-musa <awabmusaabdalla@gmail.com>
2026-04-12 22:08:55 +0200

    Implement late fee calculation logic

--- ResearchPaper.cpp
+++ ResearchPaper.cpp
@@ -36,3 +36,19 @@ void ResearchPaper::displayInfo()
     cout << "Price: $" << price << endl;
     cout << "Status: " << (getIsBorrowed() ? "Borrowed" : "Available") << endl;
 }
+void ResearchPaper::calculateLateFee(int days)
+{
+    float fee;
+    if (days <= 7)
+    {
+        fee = days * 0.75f;
+        cout << "Late fee for research paper \"" << getTitle() << "\": $" << fee
+             << " (" << days << " days at $0.75/day)" << endl;
+    }
+    else
+    {
+        fee = (7 * 0.75f) + ((days - 7) * 1.00f);
+        cout << "Late fee for research paper \"" << getTitle() << "\": $" << fee
+             << " (7 days at $0.75/day + " << (days - 7) << " days at $1.00/day)" << endl;
+    }
+}
