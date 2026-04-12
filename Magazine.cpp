945ccb839f9727e7ae9c5909b8cbf7687525cd60
awab-musa <awabmusaabdalla@gmail.com>
2026-04-12 21:24:10 +0200

    Implement Magazine constructor

--- Magazine.cpp
+++ Magazine.cpp
@@ -1,3 +1,7 @@
 #include "Magazine.h"
 #include < iostream>
 using namespace std;
+Magazine::Magazine(int id, string title, float price, int issueNumber, string publisher)
+    : LibraryItem(id, title, price), issueNumber(issueNumber), publisher(publisher)
+{
+}
